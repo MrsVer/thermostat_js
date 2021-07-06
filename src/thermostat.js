7589e27e7d1862bb456ef2843bd49f22344d2d60
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-07-06 15:46:51 +0100

    have a fully-tested thermostat

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,13 +4,23 @@ class Thermostat {
     constructor() {
     this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
     }
     getCurrentTemperature() {
       return this.temperature;
     }
     up() {
-      this.temperature += 1
-    }
+        if (this.isMaximumTemperature()) {
+          return;
+        }
+        this.temperature += 1;
+      }
     down() {
         if (this.isMinimumTemperature()) {
           return;
@@ -19,6 +29,33 @@ class Thermostat {
       }
     isMinimumTemperature() {
         return this.temperature === this.MINIMUM_TEMPERATURE;
+      }
+    isPowerSavingModeOn() {
+        return this.powerSavingMode === true;
+    }
+    switchPowerSavingModeOff() {
+        this.powerSavingMode = false;
+      }
+    switchPowerSavingModeOn() {
+        this.powerSavingMode = true;
+      }
+    isMaximumTemperature() {
+        if (this.isPowerSavingModeOn() === false) {
+          return this.temperature === this.MAX_LIMIT_PSM_OFF;
+        }
+        return this.temperature === this.MAX_LIMIT_PSM_ON;
+      }
+      resetTemperature() {
+        this.temperature = this.DEFAULT_TEMPERATURE;
+      }
+      energyUsage() {
+        if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+          return 'low-usage';
+        }
+        if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+          return 'medium-usage';
+        }
+        return 'high-usage';
       }
   }
   
