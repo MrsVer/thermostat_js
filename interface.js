1f0d3222acf3101567f2dfb98977001b8521d77b
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-07-08 16:01:16 +0100

    Connect the page with weather API

--- interface.js
+++ interface.js
@@ -34,3 +34,19 @@ document.addEventListener("DOMContentLoaded", () => {
       updateTemperature();
     })
   });
+
+  document.querySelector('#select-city').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = document.querySelector('#current-city').value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+  
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
+  })
+
+  
